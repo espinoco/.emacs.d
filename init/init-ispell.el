9a9c4b711b8bbc6cca28a0bf3638a117a9d1ac46
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-01-24 00:53:36 -0600

    Ispell spanish option, english instead of default dictionary, switch spanish-english and vice versa function

--- init/init-ispell.el
+++ init/init-ispell.el
@@ -1,3 +1,21 @@
 (setq-default ispell-program-name "/usr/local/bin/hunspell")
+(setq ispell-dictionary "english")
+;; Add spanish dictionary to the list
+(require 'ispell)
+(add-to-list 'ispell-local-dictionary-alist
+             '("spanish"
+               "[a-zA-Z\304\326\334\344\366\337\374]"
+               "[^a-zA-Z\304\326\334\344\366\337\374]"
+               "[']" t ("-C" "-d" "spanish") "~latin1" iso-8859-1))
+
+(defun switch-dictionary()
+  (interactive)
+  (let* ((dic ispell-current-dictionary)
+    	 (change (if (string= dic "spanish") "english" "spanish")))
+    (ispell-change-dictionary change)
+    (message "Dictionary switched from %s to %s" dic change)
+    ))
+
+(global-set-key (kbd "<f8>")   'switch-dictionary)
 
 (provide 'init-ispell)
