cfcbe2248ab7794bf927fcf4833843dcf2974727
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-02-08 19:19:33 -0600

    Color theme fixes between light and dark solarized themes

--- init/init-theme.el
+++ init/init-theme.el
@@ -1,4 +1,21 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'solarized-dark t)
+
+(defun set-solarized-dark-theme ()
+  (interactive)
+  (custom-set-faces
+   '(helm-match ((t (:inherit match :background "black"))))
+   '(helm-selection ((t (:background "navy" :underline t))))
+   '(yas/field-highlight-face ((t (:background "dark blue")))))
+  (load-theme 'solarized-dark t))
+
+(defun set-solarized-light-theme ()
+  (interactive)
+  (custom-set-faces
+   '(helm-match ((t (:inherit match))))
+   '(helm-selection ((t (:background "#b5ffd1" :underline t))))
+   '(yas/field-highlight-face ((t (:background unspecified)))))
+  (load-theme 'solarized-light t))
+
+(set-solarized-dark-theme)
 
 (provide 'init-theme)
