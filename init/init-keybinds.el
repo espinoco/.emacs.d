9177b8ba38c891c9ff83a9bb99958711bd16c437
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-02-09 19:34:52 -0600

    Paste back, insert quotation marks, backward-word keybinds

--- init/init-keybinds.el
+++ init/init-keybinds.el
@@ -1,5 +1,4 @@
 ;; Mark-work instead of M-@, since spanish MBP keyboard do not allows it
-(global-set-key (kbd "C-M-2") 'mark-word)
 
 (global-set-key (kbd "C-'") 'undo)
 
@@ -8,5 +7,16 @@
 (global-set-key (kbd "C-c w") 'ispell-word)
 
 (global-set-key (kbd "C-c s") 'shell)
+
+(global-set-key (kbd "C-M-y") (lambda ()
+                                (interactive)
+                                (yank-pop -1)))
+
+(global-set-key (kbd "M-s") 'backward-word)
+
+(global-set-key (kbd "C-2") (lambda ()
+                              (interactive)
+                              (insert "\"\"")
+                              (backward-char)))
 
 (provide 'init-keybinds)
