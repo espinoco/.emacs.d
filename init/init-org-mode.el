30e220051d2556533ec6d4162bd4c93ee2c05717
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-02-22 01:44:16 -0600

    Add push for org files to remote repository for version control management, add set runtime and set priority to tasks on org mode

--- init/init-org-mode.el
+++ init/init-org-mode.el
@@ -23,14 +23,37 @@
 ;; layout at start-up I close other windows.
 (delete-other-windows)
 
-;; (add-hook 'kill-emacs-hook (lambda ()
-                             ;; (interactive)
-                             ;; (shell-command "cd ~/Org" nil nil)
-                             ;; (shell-command "git add -A" nil nil)
-                             ;; (shell-command (format "git commit -m \"%s Emacs kill push\""
-                                                    ;; (shell-command "date" nil nil)) nil nil)
-                             ;; (shell-command "git push -u origin master") nil nil))
+(add-hook 'kill-emacs-hook (lambda ()
+                              (interactive)
+                              (shell-command "cd ~/Org && git add -A && git commit -m \"Emacs kill push\" && git push -u origin master" nil nil)))
 
-(defun push-org-notes)
+(defun push-org-notes ()
+  (interactive)
+  (shell-command "cd ~/Org && git add -A && git commit -m \"Emacs manually puched notes\" && git push -u origin master" nil nil))
+
+(defun set-priority (selected-priority)
+  (interactive "nEnter priority (Blocker: 1, Critical: 2, Major: 3, Minor: 4, Trivial: 5): ")
+  (if (< selected-priority 1) (error "Enter an integer number between 0 and 6")
+    (if (> selected-priority 5) (error "Enter an integer number between 0 and 6")
+      (search-forward "]")
+      (forward-char)
+      (insert (format "[#%d] " selected-priority)))))
+
+(define-key org-mode-map (kbd "C-c p") 'set-priority)
+
+(defun set-runtime (selected-runtime)
+  (interactive "sEnter runtime (Quick: A, Medium: B, Long: C, Overlong: D): ")
+  (org-beginning-of-line)
+  (forward-word)
+  (backward-word)
+  (insert " ")
+  (backward-char)
+  (insert (format "[#%s" selected-runtime))
+  (backward-char)
+  (capitalize-word 1)
+  (insert "]")
+  (org-beginning-of-line))
+
+(define-key org-mode-map (kbd "C-c r") 'set-runtime)
 
 (provide 'init-org-mode)
