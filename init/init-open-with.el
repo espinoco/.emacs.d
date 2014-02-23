020c3d0e77148a3580def1b3c689a71d258ef144
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-02-22 19:06:10 -0600

    Delete init open with, add path from shell mode

--- init/init-open-with.el
+++
@@ -1,7 +0,0 @@
-(defun open-file-with-external-program (file-name)
-  "Opens a file by directly calling the default external program"
-  (interactive (list (read-file-name "Open file: ")))
-  (shell-command (format "open \"%s\""
-                         file-name) nil nil))
-
-(provide 'init-open-with)
---
+++ init/init-path.el
@@ -0,0 +1,12 @@
+(defun set-exec-path-from-shell-PATH ()
+  "Sets the exec-path to the same value used by the user shell"
+  (let ((path-from-shell
+         (replace-regexp-in-string
+          "[[:space:]\n]*$" ""
+          (shell-command-to-string "$SHELL -l -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
+
+(provide 'init-path)
