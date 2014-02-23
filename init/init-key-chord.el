dea9ed4b987d3e1c2e67aa9387eef51b893118ab
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-02-22 18:58:12 -0600

    Keys for export latex to pdf, open video with vlc and open file with external program functions

--- init/init-key-chord.el
+++ init/init-key-chord.el
@@ -18,5 +18,8 @@
 (key-chord-define-global "x3" 'split-window-right)
 (key-chord-define-global "x0" 'delete-window)
 (key-chord-define-global "jj" 'er/expand-region)
+(key-chord-define dired-mode-map "oe" 'open-file-with-external-program)
+(key-chord-define dired-mode-map "pv" 'play-video-file-with-vlc)
+(key-chord-define dired-mode-map "ep" 'export-latex-to-pdf)
 
 (provide 'init-key-chord)
