1242740af013633355343ba9baea7b22164e8854
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-03-18 14:07:09 -0600

    Add multiple cursors functions to key binds

--- init/init-multiple-cursors.el
+++ init/init-multiple-cursors.el
@@ -3,5 +3,7 @@
 (global-set-key (kbd "C-<") 'mc/mark-next-like-this)
 (global-set-key (kbd "C->") 'mc/skip-to-next-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c C-r") 'mc/mark-sgml-tag-pair)
+(global-set-key (kbd "C-c l") 'mc/edit-beginnings-of-lines)
 
 (provide 'init-multiple-cursors)
