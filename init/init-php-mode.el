9ba70c70a41eddfe5ef7fd316b814d04c7b4ebe6
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-03-23 20:48:41 -0600

    Auto load php mode

--- init/init-php-mode.el
+++ init/init-php-mode.el
@@ -1,6 +1,10 @@
 (add-to-list 'load-path "~/.emacs.d/extensions/php")
 (require 'php-mode)
-(add-to-list 'auto-mode-alist
-     	     '("\\.php[34]?\\'\\|\\.phtml\\'" . php-mode))
+
+(autoload 'php-mode "php-mode"
+  "Major mode for editing php code." t)
+
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
 
 (provide 'init-php-mode)
