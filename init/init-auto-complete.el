d605005151ed337c59d072b0b3709f26882209f0
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-03-26 15:50:36 -0600

    Auto complete boost performance

--- init/init-auto-complete.el
+++ init/init-auto-complete.el
@@ -3,12 +3,16 @@
 (ac-config-default)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/extensions/auto-complete/ac-dict")
 
-;; Select candidates with C-n/C-p only when completion menu is displayed
 (setq ac-use-menu-map t)
 (define-key ac-menu-map "\C-n" 'ac-next)
 (define-key ac-menu-map "\C-p" 'ac-previous)
 
 (add-to-list 'ac-modes 'html-mode)
 (add-to-list 'ac-modes 'nxml-mode)
+
+(setq ac-auto-show-menu 0.1
+      ac-menu-height 5
+      ac-auto-start 3
+      ac-delay 0)
 
 (provide 'init-auto-complete)
