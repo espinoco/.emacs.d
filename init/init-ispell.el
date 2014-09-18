edc783136badb41387287af22c9b38f1afb341d1
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-09-18 16:03:28 -0500

    Default dictionary to English, utf-8 encoding
    Previous default dictionary was Spanish, but is more often used English,
    the English dictionary is set as ispell-dictionary, run "hunspell -D"
    and check AVAILABLE DIRECTORIES, there must be a default dictionary,
    with files named default.aff and default.dic.
    
    ERROR: ispell-phaf: No matching entry for nil.

--- init/init-ispell.el
+++ init/init-ispell.el
@@ -1,22 +1,32 @@
-(setq-default ispell-program-name "/usr/local/bin/hunspell")
-(setq ispell-dictionary "spanish")
-
-;; Add spanish dictionary to the list
 (require 'ispell)
+
+(setq ispell-program-name "/usr/local/bin/hunspell"
+      ispell-really-hunspell t
+      ispell-dictionary "english")
+
+(add-to-list 'ispell-local-dictionary-alist
+             '("english"
+               "[[:alpha:]]"
+               "[^[:alpha:]]"
+               "[']"
+               t
+               ("-d" "english")
+               nil utf-8))
+
 (add-to-list 'ispell-local-dictionary-alist
              '("spanish"
                "[a-zA-Z\304\326\334\344\366\337\374]"
                "[^a-zA-Z\304\326\334\344\366\337\374]"
-               "[']" t ("-C" "-d" "spanish") "~latin1" iso-8859-1))
+               "[']" t ("-C" "-d" "spanish") "~latin1" utf-8))
 
-(defun switch-dictionary()
+(defun switch-dictionary ()
   (interactive)
   (let* ((dic ispell-current-dictionary)
-    	 (change (if (string= dic "spanish") "english" "spanish")))
+         (change (if (string= dic "english") "spanish" "english")))
     (ispell-change-dictionary change)
     (message "Dictionary switched from %s to %s" dic change)
     ))
 
-(global-set-key (kbd "<f8>")   'switch-dictionary)
+(global-set-key (kbd "<f8>") 'switch-dictionary)
 
 (provide 'init-ispell)
