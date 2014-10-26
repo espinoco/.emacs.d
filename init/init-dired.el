d9f2fc8d6e032985ec8b86d2758100e46daba414
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-10-26 16:16:21 -0600

    Hide details in dired

--- init/init-dired.el
+++ init/init-dired.el
@@ -1,5 +1,6 @@
 ;; Dired Jump
 (autoload 'dired-jump "dired-x"
   "Jump to Dired buffer corresponding to current buffer." t)
+(add-hook 'dired-mode-hook 'dired-hide-details-mode)
 
 (provide 'init-dired)
