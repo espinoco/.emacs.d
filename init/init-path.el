db1c429e03b42802883a18f866ada1205f3422d1
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-10-30 17:17:29 -0600

    Path fix
    Playing around with homebrew and setting fish shell as default shell
    lead to modification on path so emacs was unable to find programs as ls,
    so this fix path was applied.

--- init/init-path.el
+++ init/init-path.el
@@ -1,12 +1,4 @@
-(defun set-exec-path-from-shell-PATH ()
-  "Sets the exec-path to the same value used by the user shell"
-  (let ((path-from-shell
-         (replace-regexp-in-string
-          "[[:space:]\n]*$" ""
-          (shell-command-to-string "$SHELL -l -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
-
-(set-exec-path-from-shell-PATH)
+(setenv "PATH" (concat (getenv "PATH") ":/bin"))
+(setq exec-path (append exec-path '("/bin")))
 
 (provide 'init-path)
