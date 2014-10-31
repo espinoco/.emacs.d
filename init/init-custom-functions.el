c5dc385b42bef41f7994ca4c91ff4fe6048ad466
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-10-30 21:04:27 -0600

    Add open-file-with-external-program

--- init/init-custom-functions.el
+++ init/init-custom-functions.el
@@ -39,12 +39,22 @@ vi style of % jumping to matching brace."
         ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
         (t (self-insert-command (or arg 1)))))
 
-(defun open-file-with-external-program ()
-  "Opens a file by directly calling the default external program"
-  (interactive)
-  (defvar get-filename-on-dired 'dired-copy-filename-as-kill)
-  (shell-command (format "open \"%s\""
-                         (funcall get-filename-on-dired))))
+(defun open-file-with-external-program (arg)
+  "Open visited file in default external program.
+   When in dired mode, open file under the cursor.
+   With a prefix ARG always prompt for command to use."
+  (interactive "P")
+  (let* ((current-file-name
+          (if (eq major-mode 'dired-mode)
+              (dired-get-file-for-visit)
+            buffer-file-name))
+         (open (pcase system-type
+                 (`darwin "open")
+                 ((or `gnu `gnu/linux `gnu/kfreebsd) "xdg-open")))
+         (program (if (or arg (not open))
+                      (read-shell-command "Open current file with: ")
+                    open)))
+    (start-process "open-file-with-external-program" nil program current-file-name)))
 
 (defun export-latex-to-pdf ()
   "Exports latex file to pdf with pdflatex"
