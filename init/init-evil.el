eb7074757dd299f617eb04c0e2d733fd8565f477
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-10-30 21:54:43 -0600

    Evil multiple-cursors handle
    Evil does not play nice with multiple-cursors

--- init/init-evil.el
+++ init/init-evil.el
@@ -8,4 +8,22 @@
 (setq evilnc-hotkey-comment-operator ",,")
 (require 'evil-nerd-commenter)
 (evilnc-default-hotkeys)
+(defvar my-mc-evil-previous-state nil)
+(defun my-mc-evil-switch-to-emacs-state ()
+  (when (and (bound-and-true-p evil-mode)
+             (not (eq evil-state 'emacs)))
+    (setq my-mc-evil-previous-state evil-state)
+    (evil-emacs-state)))
+(defun my-mc-evil-back-to-previous-state ()
+  (when my-mc-evil-previous-state
+    (unwind-protect
+        (case my-mc-evil-previous-state
+          ((normal visual insert) (evil-force-normal-state))
+          (t (message "Don't know how to handle previous state: %S"
+                      my-mc-evil-previous-state)))
+      (setq my-mc-evil-previous-state nil))))
+(add-hook 'multiple-cursors-mode-enabled-hook
+          'my-mc-evil-switch-to-emacs-state)
+(add-hook 'multiple-cursors-mode-disabled-hook
+          'my-mc-evil-back-to-previous-state)
 (provide 'init-evil)
