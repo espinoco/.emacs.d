f746aa8a1d11a267d63862b4945ea2536a5b6ccd
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-11-04 00:09:38 -0600

    Set default tramp method to ssh

--- init/init-tramp.el
+++ init/init-tramp.el
@@ -2,4 +2,6 @@
                                '((tramp-parse-sconfig "/etc/ssh_config")
                                  (tramp-parse-sconfig "~/.ssh/config")))
 
+(setq tramp-default-method "ssh")
+
 (provide 'init-tramp)
