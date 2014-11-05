dd47f6723aa9ec31f38bf579d82ed1bd73c5464b
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-11-05 12:56:26 -0600

    Add window move like vim

--- init/init-smartrep.el
+++ init/init-smartrep.el
@@ -1,2 +1,15 @@
 (require 'smartrep)
+
+(global-set-key "\C-w" nil)
+
+(smartrep-define-key
+    global-map "C-w" '(("q" . 'delete-window)
+                       ("o" . 'delete-other-windows)
+                       ("s" . 'switch-to-buffer-other-bottom-window)
+                       ("v" . 'switch-to-buffer-other-side-window)
+                       ("k" . 'windmove-up)
+                       ("j" . 'windmove-down)
+                       ("h" . 'windmove-left)
+                       ("l" . 'windmove-right)))
+
 (provide 'init-smartrep)
