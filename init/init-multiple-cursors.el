cfbce602f69f40865e0547c2d66d10ed63e723ba
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-11-15 17:08:13 -0600

    Update multiple-cursors init file

--- init/init-multiple-cursors.el
+++ init/init-multiple-cursors.el
@@ -1,9 +1,5 @@
 (setq mc/list-file (concat savefile-dir "/.mc-lists.el"))
+
 (require 'multiple-cursors)
-(global-set-key (kbd "C-<") 'mc/mark-next-like-this)
-(global-set-key (kbd "C->") 'mc/skip-to-next-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-(global-set-key (kbd "C-c C-r") 'mc/mark-sgml-tag-pair)
-(global-set-key (kbd "C-c l") 'mc/edit-beginnings-of-lines)
 
 (provide 'init-multiple-cursors)
