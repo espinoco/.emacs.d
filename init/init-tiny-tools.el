cf94057d40b19027475b56543113388c856d8a06
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-11-15 17:47:27 -0600

    Update tiny tools init file

--- init/init-tiny-tools.el
+++ init/init-tiny-tools.el
@@ -1,21 +1,5 @@
 (require 'tinyeat)
 
-(global-set-key (kbd "<M-backspace>") 'tinyeat-backward-preserve)
-
-(global-set-key "\M-d" 'tinyeat-forward-preserve)
-
 (global-set-key (kbd "<S-backspace>") 'tinyeat-delete-whole-word)
-
-(global-set-key (kbd "C-k") 'tinyeat-kill-line)
-
-(global-set-key (kbd "C-S-SPC") 'tinyeat-join-lines)
-
-(global-set-key (kbd "<C-backspace>") (lambda ()
-                                        (interactive)
-                                        (forward-word 2)
-                                        (backward-word)
-                                        (tinyeat-delete-whole-word)
-                                        (tinyeat-backward-preserve)
-                                        (backward-word)))
 
 (provide 'init-tiny-tools)
