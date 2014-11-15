66c03e1d301166437be3ecb8fd3533c720bbdff3
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-11-15 17:33:59 -0600

    Update highlith symbol init file

--- init/init-highlight-symbol.el
+++ init/init-highlight-symbol.el
@@ -1,7 +1,11 @@
 (require 'highlight-symbol)
+
 (global-set-key [(control f9)] 'highlight-symbol-at-point)
+
 (global-set-key [f9] 'highlight-symbol-next)
+
 (global-set-key [(shift f9)] 'highlight-symbol-prev)
+
 (global-set-key [(meta f9)] 'highlight-symbol-query-replace)
 
 (provide 'init-highlight-symbol)
