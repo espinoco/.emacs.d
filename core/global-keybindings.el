5b2ed6dcb7b42cf28c5fee232bc6f231635efce7
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-11-15 15:21:55 -0600

    Move tiny tools keybinds

--- core/global-keybindings.el
+++ core/global-keybindings.el
@@ -2,24 +2,6 @@
                                 (interactive)
                                 (yank-pop -1)))
 
-(global-set-key (kbd "<M-backspace>") 'tinyeat-backward-preserve)
-
-(global-set-key "\M-d" 'tinyeat-forward-preserve)
-
-(global-set-key (kbd "<S-backspace>") 'tinyeat-delete-whole-word)
-
-(global-set-key (kbd "C-k") 'tinyeat-kill-line)
-
-(global-set-key (kbd "C-S-SPC") 'tinyeat-join-lines)
-
-(global-set-key (kbd "<C-backspace>") (lambda ()
-                                        (interactive)
-                                        (forward-word 2)
-                                        (backward-word)
-                                        (tinyeat-delete-whole-word)
-                                        (tinyeat-backward-preserve)
-                                        (backward-word)))
-
 (global-set-key (kbd "C-o") 'insert-and-indent-line-below)
 
 (global-set-key (kbd "C-S-o") 'insert-and-indent-line-above)
