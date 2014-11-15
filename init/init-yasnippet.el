3559b50716f742b08110e46fdc92607cdd67dc80
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-11-15 17:43:39 -0600

    Fix yas-reload-all

--- init/init-yasnippet.el
+++ init/init-yasnippet.el
@@ -1,7 +1,8 @@
 (require 'yasnippet)
-(yas-global-mode 1)
 
 ;; The latest yasnippet and ac-source-yasnippet did not work together
 (setq ac-source-yasnippet nil)
+
+(yas-reload-all)
 
 (provide 'init-yasnippet)
