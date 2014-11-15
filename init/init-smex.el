db16bee8bb54fbc57dedaf5604e85db6accbf477
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-11-15 16:51:29 -0600

    Add savefile to savefile-dir

--- init/init-smex.el
+++ init/init-smex.el
@@ -1,6 +1,10 @@
 (require 'smex)
+
+(setq smex-save-file (concat savefile-dir "/.smex-items"))
+
 (setq smex-history-length 14
       smex-prompt-string "=> ")
+
 (smex-initialize)
 
 (provide 'init-smex)
