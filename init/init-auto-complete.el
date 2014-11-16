e08e2cdc1c81de268e32fe5451818056e33f1f1f
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-11-15 18:54:02 -0600

    Update init-auto-complete

--- init/init-auto-complete.el
+++ init/init-auto-complete.el
@@ -1,17 +1,33 @@
 (require 'auto-complete-config)
+
 (ac-config-default)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/extensions/auto-complete/ac-dict")
+
+(add-to-list 'ac-dictionary-directories (concat emacs-dir "modules/auto-complete/ac-dict"))
 
 (setq ac-use-menu-map t)
-(define-key ac-menu-map "\C-n" 'ac-next)
-(define-key ac-menu-map "\C-p" 'ac-previous)
+
+(define-key ac-menu-map "\C-j" 'ac-next)
+
+(define-key ac-menu-map "\C-k" 'ac-previous)
 
 (add-to-list 'ac-modes 'html-mode)
+
 (add-to-list 'ac-modes 'nxml-mode)
 
 (setq ac-auto-show-menu 0
-      ac-menu-height 3
+      ac-menu-height 4
       ac-auto-start 2
-      ac-delay 0)
+      ac-delay 0
+      ac-candidate-limit 4)
+
+(set-face-background 'ac-candidate-face "#2c2e2e")
+
+(set-face-foreground 'ac-candidate-face "#9f9f9f")
+
+(ac-set-trigger-key "TAB")
+
+(define-key ac-completing-map "\C-c" 'ac-stop)
+
+(setq ac-use-quick-help nil)
 
 (provide 'init-auto-complete)
