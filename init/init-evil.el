e0a186dfdb19c4ba7181e1094c4e6c67d25329ef
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2014-11-17 22:44:24 -0600

    Remove useless comment

--- init/init-evil.el
+++ init/init-evil.el
@@ -39,26 +39,6 @@
 
 (define-key evil-insert-state-map (kbd "C-c") 'my-evil-force-normal-state)
 
-;; (defvar my-mc-evil-previous-state nil)
-;; (defun my-mc-evil-switch-to-emacs-state ()
-;;   (when (and (bound-and-true-p evil-mode)
-;;              (not (eq evil-state 'emacs)))
-;;     (setq my-mc-evil-previous-state evil-state)
-;;     (evil-emacs-state)))
-;; (defun my-mc-evil-back-to-previous-state ()
-;;   (when my-mc-evil-previous-state
-;;     (unwind-protect
-;;         (case my-mc-evil-previous-state
-;;           ((normal visual insert) (evil-force-normal-state))
-;;           (t (message "Don't know how to handle previous state: %S"
-;;                       my-mc-evil-previous-state)))
-;;       (setq my-mc-evil-previous-state nil))))
-;; (add-hook 'multiple-cursors-mode-enabled-hook
-;;           'my-mc-evil-switch-to-emacs-state)
-;; (add-hook 'multiple-cursors-mode-disabled-hook
-;; (add-hook 'multiple-cursors-mode-enabled-hook 'evil-emacs-state)
-;; (add-hook 'multiple-cursors-mode-disabled-hook 'evil-normal-state)
-;; Don't move back the cursor one position when exiting insert mode
 (define-key evil-normal-state-map "m" 'point-to-register)
 
 (define-key evil-normal-state-map "'" 'jump-to-register)
