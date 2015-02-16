c1add81acc6cbc3af989022880fe95c00cb42679
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2015-02-16 14:39:33 -0600

    Add mac functions for copy and paste
    Only without window system

--- core/os-x.el
+++ core/os-x.el
@@ -1,3 +1,21 @@
 (setq mac-command-modifier 'control)
 
+(defun mac-copy ()
+  (shell-command-to-string "pbpaste"))
+
+(defun mac-paste (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(defun set-mac-paste-and-copy ()
+  "Sets mac functions for copy and paste inside terminal"
+  (setq interprogram-cut-function 'mac-paste)
+  (setq interprogram-paste-function 'mac-copy))
+
+(if window-system
+    (message "Using window system")
+  (set-mac-paste-and-copy))
+
 (provide 'os-x)
