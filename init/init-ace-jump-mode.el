811707a8dc2c53db2f58b1b41cf384887f12868c
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2015-02-16 13:31:31 -0600

    Set ace-jump-mode attribute faces

--- init/init-ace-jump-mode.el
+++ init/init-ace-jump-mode.el
@@ -26,4 +26,13 @@
 
 (setq ace-jump-mode-case-fold nil)
 
+(require 'ace-jump-mode)
+
+(set-face-attribute 'ace-jump-face-background nil
+                    :foreground "#7a7a7a")
+
+(set-face-attribute 'ace-jump-face-foreground nil
+                    :weight 'bold
+                    :foreground "#ecd99e")
+
 (provide 'init-ace-jump-mode)
