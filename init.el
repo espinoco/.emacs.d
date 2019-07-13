d10b2f636f80c47ccab9f2947a9448cf10fe634b
Carlo Eduardo Rodriguez Espino <v-crodriguezespi@expedia.com>
2019-07-12 23:02:55 -0500

    fix tide on web mode

--- init.el
+++ init.el
@@ -442,9 +442,14 @@
   (add-hook 'typescript-mode-hook #'setup-tide-mode)
   ;; TSX
   (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.ts\\'" . web-mode))
   (add-hook 'web-mode-hook
     (lambda ()
       (when (string-equal "tsx" (file-name-extension buffer-file-name))
+        (setup-tide-mode))))
+  (add-hook 'web-mode-hook
+    (lambda ()
+      (when (string-equal "ts" (file-name-extension buffer-file-name))
         (setup-tide-mode))))
   (flycheck-add-mode 'typescript-tslint 'web-mode)
   ;; JavaScript
