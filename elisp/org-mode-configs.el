5acd245e25b39b9dfc9847bf9e8422474931f2a5
Carlo Eduardo Rodriguez Espino <carloeduardorodriguez@gmail.com>
2019-11-18 11:47:11 -0600

    Change namge of week file to projects

--- elisp/org-mode-configs.el
+++ elisp/org-mode-configs.el
@@ -1,15 +1,15 @@
 (setq
     org-directory "~/gtd"
-    org-agenda-files '("~/gtd/week.org" "~/gtd/waiting.org")
+    org-agenda-files '("~/gtd/projects.org" "~/gtd/waiting.org")
     org-capture-templates '(("e" "entries" entry (file "~/gtd/entries.org")
                                 "* TODO %i%?")
                                ("w" "waiting" entry (file "~/gtd/waiting.org")
                                    "* %i%?\n %U")
-                               ("k" "week" entry (file "~/gtd/week.org")
+                               ("p" "projects" entry (file "~/gtd/projects.org")
                                    "* TODO %i%?")
                                ("b" "backlog" entry (file "~/gtd/backlog.org")
                                    "* %i%?"))
-    org-refile-targets '(("~/gtd/week.org" :maxlevel . 3)
+    org-refile-targets '(("~/gtd/projects.org" :maxlevel . 3)
                             ("~/gtd/backlog.org" :level . 1)
                             ("~/gtd/waiting.org" :maxlevel . 2))
     org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)"))
